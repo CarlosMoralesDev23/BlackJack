62c592e292c45d358ba67de44e2406b91770dedc
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-02-13 01:56:52 -0300

    Se refactorizo la funcion puntaje-carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,7 +5,6 @@
  * 2S = Two of Spades
  */
 
-console.log(!isNaN("10"));
 let especiales = ["J", "K", "Q", "A"];
 let tipos = ["C", "D", "H", "S"];
 let deck = [];
@@ -13,7 +12,8 @@ let cartasJugador = []
 let cartasComputador = []
 let puntosJugador = 0;
 let puntosComputador = 0;
-// ---------------- Crear Deck ----------------
+
+// ---------------- 1- Crear Deck ----------------
 let crearDeck = () => {
     for (i = 2; i <= 10; i++) {
         for (let tipo of tipos) {
@@ -32,49 +32,37 @@ crearDeck()
 console.log(deck);
 
 
-// -------------  Puntaje Carta ------------------
-let carta = 0
-
-let puntaje = () => {
-    carta = carta.slice(0, -1);
-    console.log(`Este es el resto sacandole la letra : ${carta}`)
-    if (isNaN(carta)) {
-        carta = 10;
-        sumaPuntaje(carta)
-    } else {
-        carta = Number(carta);
-        sumaPuntaje(carta)
-    }
-    console.log(`Valor de la carta: ${carta}`);
-};
-
-
-
-// -------------  Sumar Puntaje ------------------
-
-let sumaPuntaje = (carta)=>{
-    puntosJugador += carta
-    console.log(`Puntos del jugador: ${puntosJugador}`);
-}
-
-
-// -------------  Pedir Carta ------------------
+// ------------- 2- Pedir Carta ------------------
 let pedirCarta = () => {
     if ( deck.length === 0){
         throw 'No hay mas cartas en la baraja'
     }
     carta = deck.shift()
-    console.log(deck);
     cartasJugador.push(carta)
-
-    puntaje()
+    console.log(cartasJugador);
     return carta
 }
-// deck= []   prueba la condicional si el deck esta vacio
-pedirCarta(deck) 
-pedirCarta(deck) 
-pedirCarta(deck) 
-console.log(cartasJugador)
+
+
+
+// ------------- 3- Puntaje Carta ------------------
+let valorCarta = (carta) => {
+    const valor = carta.slice(0, -1);
+    return (isNaN(valor)) ? ( valor === 'A' ? 11 :10): valor*1; //String to Number
+};
+const valor = valorCarta( pedirCarta() )
+console.log(valor)
+
+
+// -------------  Sumar Puntaje ------------------
+
+// let sumaPuntaje = (puntos)=>{
+//     puntosJugador += puntos
+//     console.log(`Puntos del jugador: ${puntosJugador}`);
+// }
+
+
+
 
 
 
