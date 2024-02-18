8b38825d43fe48e8371d1407bb964c018ca0a1c2
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-02-18 00:58:45 -0300

    Se implemento funcionalidad de nueva partidad

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,27 +1,13 @@
-/**
- * 2C = Two of clubs
- * 2D = Two of Diamonds
- * 2H = Two of Hearts
- * 2S = Two of Spades
- */
+/**  * 2C = Two of clubs, * 2D = Two of Diamonds, * 2H = Two of Hearts,  * 2S = Two of Spades */
 
 // alert('Haz click en nuevo juego para comenzar, luego en pedir carta, y detener para pasar el turno a la computadora, reinicia navegador para una nueva partida')
 // alert('Ganas si sumas 21, te puedes detener antes y ver si la compu empata, gana o pierde')
 let especiales = ["J", "K", "Q", "A"];
 let tipos = ["C", "D", "H", "S"];
 let deck = [];
-let cartasJugador = [];
-let cartasComputador = [];
 let pJugador = 0;
 let pCompu = 0;
-let btnNuevo         = document.getElementById("nuevoJuego");
-let btnPedir         = document.getElementById("pedirCarta");
-let smallPuntos      = document.querySelectorAll("small");
-let btnDetener       = document.getElementById("detenerTurno");
-let divCartasJugador = document.getElementById('divCartasJugador')
-let divCartasComputador = document.getElementById('divCartasComputador')
 
-// ---------------- 1- Crear Deck ----------------
 let crearDeck = () => {
     for (i = 2; i <= 10; i++) {
         for (let tipo of tipos) {
@@ -37,17 +23,14 @@ let crearDeck = () => {
     return deck;
 };
 
-// ------------- 2- Pedir Carta ------------------
 let pedirCarta = () => {
     if (deck.length === 0) {
         throw "No hay mas cartas en la baraja";
     }
     carta = deck.shift();
-    cartasJugador.push(carta);
     return carta;
 };
 
-// ------------- 3- Puntaje Carta ------------------
 let valorCarta = (carta) => {
     const valor = carta.slice(0, -1);
     return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1; //String to Number
@@ -57,7 +40,7 @@ let valorCarta = (carta) => {
 btnPedir.addEventListener('click', ()=>{
     const carta = pedirCarta()
     pJugador += valorCarta(carta)
-    smallPuntos[0].innerText = pJugador
+    smallJugador.innerText = pJugador
 
     //Esto va ser una fucnión insertar Carta
     const imgCarta = document.createElement("img");
@@ -85,35 +68,43 @@ btnDetener.addEventListener("click", () => {
     turnoComputador(pJugador)
 })
 
-
 btnNuevo.addEventListener('click', ()=>{
-    crearDeck()
+    console.clear()
+    deck     = [];
+    pJugador = 0;
+    pCompu = 0;
+    smallCompu.innerText       = "0";
+    smallJugador.innerText     = "0";
+    divCartasCompu.innerHTML   = "";
+    divCartasJugador.innerHTML = "";
+    btnDetener.disabled        = false;
+    btnPedir.disabled          = false;
+
+    deck = crearDeck();
 })
 
-
-
 const turnoComputador = (pjugador)=>{
-        do{
-            const carta = pedirCarta();
-            pCompu += valorCarta(carta);
-            smallPuntos[1].innerText = pCompu;
+    do{
+        const carta = pedirCarta();
+        pCompu += valorCarta(carta);
+        smallCompu.innerText = pCompu;
 
-            const imgCarta = document.createElement("img");
-            imgCarta.src = `./assets/cartas/cartas/${carta}.png`;
-            imgCarta.classList.add("cartas");
-            divCartasCompu.append(imgCarta);
+        const imgCarta = document.createElement("img");
+        imgCarta.src = `./assets/cartas/cartas/${carta}.png`;
+        imgCarta.classList.add("cartas");
+        divCartasCompu.append(imgCarta);
 
-            pCompu === pJugador
-                ? console.warn("El computador igualo tus puntos perdiste")
-                : pCompu > pJugador && pCompu < 21
-                ? console.warn("El computador hizo mas puntos que tu y menos de 21  GANO el Computador")
-                : pCompu === 21
-                ? console.warn("El computador hizo 21 puntos perdiste")
-                : pCompu > 21
-                ? console.warn("El computador hizo mas de 21 puntos GANASTE")
-                : null;
-            if(pjugador > 21){
-                break
-            }
-        }while (pCompu < pJugador)
+        pCompu === pJugador
+            ? console.warn("El computador igualo tus puntos perdiste")
+            : pCompu > pJugador && pCompu < 21
+            ? console.warn("El computador hizo mas puntos que tu y menos de 21  GANO el Computador")
+            : pCompu === 21
+            ? console.warn("El computador hizo 21 puntos perdiste")
+            : pCompu > 21
+            ? console.warn("El computador hizo mas de 21 puntos GANASTE")
+            : null;
+        if(pjugador > 21){
+            break
+        }
+    }while (pCompu < pJugador)
 }
